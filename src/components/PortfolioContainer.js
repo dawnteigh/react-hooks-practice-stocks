07fbc2317f80b9db22a5355c8b4499100b03e2d3
Donte Ladatto <donte279@gmail.com>
2022-04-15 20:18:28 -0500

    commit tree b6cd10fec71d0df9fc90bcd765523b0d10a5c2f3

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({ onPortClick, stocks }) {
+
+
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      {stocks.map(stock => {
+        if (stock.inPort === true) {
+          return (
+        <Stock onPortClick={onPortClick} key={stock.id} stock={stock} />
+      )
+    }}
+    )}
     </div>
   );
 }
